ff8419cdaa39907ec7e711017399982ac6c0516a
dantas <caaddss>
2018-09-14 12:51:13 -0300

    Refactor: Importação json, nomes de variáveis e comentários

--- coletor.py
+++ coletor.py
@@ -1,36 +1,24 @@
 from twitter import *
-import json
 import pandas as pd
-
-# Tratando erro de importação do json
 try:
-    import json
-except ImportError:
     import simplejson as json
+except ImportError:
+    from json import json
 
 # Load credentials from json file
-with open("twitter_credentials.json", "r") as file:  
-    creds = json.load(file)
-
-
+with open("twitter_credentials.json", "r") as api_tokens:  
+    creds = json.load(api_tokens)
 
 # Instanciando um objeto
 credentials_tw_api = Twitter(auth=OAuth(creds['ACCESS_TOKEN'], creds['ACCESS_TOKEN_SECRET'], creds['CONSUMER_KEY'], creds['CONSUMER_SECRET']))
 
-# Criando a query
-
-query = {
-    'q':'marina silva',
-    'result_type':'popular',
-    'count':'50',
-    'lang': 'pt',
-}
-
 # Criando a busca
-busca = credentials_tw_api.search.tweets(q='marina silva', result_type='popular', count='50', lang='pt')
+search = credentials_tw_api.search.tweets(q='marina silva', result_type='popular', count='50', lang='pt')
 
-# Procurando os tweets
-dict_ = {'user': [], 'date': [], 'text':[],'favorite_count':[]}
+# Iniciando o dicionário da busca
+search_keys = {'user': [], 'date': [], 'text':[],'favorite_count':[]}
+
+# Organizando uma lista para cada chave
 for status in busca['statuses']:
     dict_['user'].append(status['user']['screen_name'])
     dict_['date'].append(status['created_at'])
